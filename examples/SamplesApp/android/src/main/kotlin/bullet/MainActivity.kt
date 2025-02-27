1fa635bb550d4fffd219e8650537cd968b167b1d
Natan <xpenatan@gmail.com>
2025-02-27 20:26:32 -0300

    fix android

--- examples/SamplesApp/android/src/main/kotlin/bullet/MainActivity.kt
+++ examples/SamplesApp/android/src/main/kotlin/bullet/MainActivity.kt
@@ -3,7 +3,7 @@ package bullet
 import android.os.Bundle
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
-import jolt.example.basic.JoltGame
+import jolt.example.samples.app.JoltGame
 
 class MainActivity : AndroidApplication() {
     override fun onCreate(savedInstanceState: Bundle?) {
