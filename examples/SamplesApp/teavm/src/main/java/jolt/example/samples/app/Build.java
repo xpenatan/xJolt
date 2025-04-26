5015752b6bef74a9c0b710e983ed3be73df60c94
Natan <xpenatan@gmail.com>
2025-04-26 08:40:08 -0300

    update main to last backend changes

--- examples/SamplesApp/teavm/src/main/java/jolt/example/samples/app/Build.java
+++ examples/SamplesApp/teavm/src/main/java/jolt/example/samples/app/Build.java
@@ -3,12 +3,11 @@ package jolt.example.samples.app;
 import com.github.xpenatan.gdx.backends.teavm.config.AssetFileHandle;
 import com.github.xpenatan.gdx.backends.teavm.config.TeaBuildConfiguration;
 import com.github.xpenatan.gdx.backends.teavm.config.TeaBuilder;
-import com.github.xpenatan.gdx.backends.teavm.gen.SkipClass;
 import java.io.File;
 import java.io.IOException;
+import org.teavm.tooling.TeaVMTargetType;
 import org.teavm.tooling.TeaVMTool;
 
-@SkipClass
 public class Build {
 
     public static void main(String[] args) throws IOException {
@@ -18,6 +17,7 @@ public class Build {
 
         TeaVMTool tool = TeaBuilder.config(teaBuildConfiguration);
         tool.setMainClass(Launcher.class.getName());
+        tool.setTargetType(TeaVMTargetType.WEBASSEMBLY_GC);
         tool.setObfuscated(false);
         TeaBuilder.build(tool);
     }
